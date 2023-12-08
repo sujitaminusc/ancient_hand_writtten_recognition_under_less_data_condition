fa978024fe73b1ec92f9e9d519866561b25a6a36
Sujit Sundar Amin <143650989+sujitaminusc@users.noreply.github.com>
2023-12-08 10:58:23 -0800

    standardized

--- utils.py
+++ utils.py
@@ -70,3 +70,20 @@ def preprocess_data(x_data, y_data):
     x_data = x_data.reshape(-1, 28, 28, 1, order="A").astype("float32") / 255.0
     y_data = to_categorical(y_data.astype("float32"))
     return x_data, y_data
+
+
+def load_images(root_folder):
+    images = []
+    labels = []
+
+    for folder_name in os.listdir(root_folder):
+        folder_path = os.path.join(root_folder, folder_name)
+
+        if os.path.isdir(folder_path):
+            for filename in os.listdir(folder_path):
+                img = cv2.imread(os.path.join(folder_path, filename))
+                if img is not None:
+                    images.append(img)
+                    labels.append(folder_name)
+
+    return np.array(images), np.array(labels)
